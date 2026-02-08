14bdc7c045d81d1dd683427ee16a6e8907ccc17a
rinks00123 <rinks00123@gmail.com>
2026-02-08 18:27:15 -0500

    update some files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,28 +1,32 @@
 import { Routes } from '@angular/router';
-import { ProductListComponent } from './components/product-list/product-list.component';
-import { ProductFormComponent } from './components/product-form/product-form.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'products', pathMatch: 'full' },
+
   {
     path: 'products',
     loadComponent: () =>
       import('./components/product-list/product-list.component').then(
         (m) => m.ProductListComponent,
       ),
+    data: { renderMode: 'client' }, // ✅ safe
   },
+
   {
     path: 'products/add',
     loadComponent: () =>
       import('./components/product-form/product-form.component').then(
         (m) => m.ProductFormComponent,
       ),
+    data: { renderMode: 'client' }, // ✅ recommended
   },
+
   {
     path: 'products/edit/:id',
     loadComponent: () =>
       import('./components/product-form/product-form.component').then(
         (m) => m.ProductFormComponent,
       ),
+    data: { renderMode: 'client' }, // ✅ REQUIRED (fixes error)
   },
 ];
